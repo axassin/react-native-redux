c3594f33c58b2f94f1014613f3223b2a62e682d2
shinchan <jiobuaron17@gmail.com>
2019-07-04 18:16:58 +0800

    Initial push to master

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off'
+  },
+  'globals': {
+    "fetch": false
+  }
+}
